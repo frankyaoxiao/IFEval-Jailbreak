ce1d7a9de63ec95ddadd955b2a50362a1d713fff
Frank Xiao <frankyaoxiao@gmail.com>
2025-10-06 18:09:22 -0700

    ckpt

--- scripts/activation/run_activation_similarity.sh
+++ scripts/activation/run_activation_similarity.sh
@@ -1,9 +1,9 @@
 # Test similarity between model responses and activation direction
 python -m src.activation_analysis.activation_similarity \
   artifacts/activation_directions/allnew.pt \
-  --model olmo7b_dpo \
+  --model olmo7b_sft \
   --prompt-set rollout_pairs \
   --num-prompts 6 \
-  --iterations 5 \
+  --iterations 50 \
   --temperature 0.7 \
   --do-sample \
